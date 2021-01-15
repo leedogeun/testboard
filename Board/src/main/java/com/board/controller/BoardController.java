59438b4639e2f98ce073be5e4c11b7e7e8377392
Chester <ehrms7989@naver.com>
2021-01-15 18:05:19 +0900

    log 진행중, 메이븐의 실수로 repostory? 지우고 다시 설치,

--- Board/src/main/java/com/board/controller/BoardController.java
+++ Board/src/main/java/com/board/controller/BoardController.java
@@ -10,12 +10,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BoardController {
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	private static final Logger boardlogger = LoggerFactory.getLogger(HomeController.class);
 
-	@RequestMapping(value = { "/board_list", "/board" }, method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("board list", locale);
+	@RequestMapping(value = { "/board_write", "/write" }, method = RequestMethod.GET)
+	public String write(Locale locale, Model model) {
+		boardlogger.info("board write", locale);
+		
+		return "board_list";
+	}
+	@RequestMapping(value = { "/board_list", "/board", "/list" }, method = RequestMethod.GET)
+	public String list(Locale locale, Model model) {
+		boardlogger.info("board list", locale);
 
+		return "board_list";
+	}
+	@RequestMapping(value = { "/board_detail", "/detail" }, method = RequestMethod.GET)
+	public String detail(Locale locale, Model model) {
+		boardlogger.info("board detail", locale);
+		
+		return "board_list";
+	}
+	@RequestMapping(value = { "/board_update", "/update" }, method = RequestMethod.GET)
+	public String update(Locale locale, Model model) {
+		boardlogger.info("board update", locale);
+		
+		return "board_list";
+	}
+	@RequestMapping(value = { "/board_delete", "/delete" }, method = RequestMethod.GET)
+	public String delete(Locale locale, Model model) {
+		boardlogger.info("board delete", locale);
+		
 		return "board_list";
 	}
 
