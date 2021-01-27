8ca49428c01daaf81b5aa83350537bcb7b17ee75
Chester <ehrms7989@naver.com>
2021-01-27 14:20:29 +0900

    수정중-1(DAO 빼는중)

--- Board/src/main/java/com/board/dao/BoardDAO.java
+++ Board/src/main/java/com/board/dao/BoardDAO.java
@@ -16,26 +16,30 @@ public class BoardDAO implements BoardDAOinterface {
 
 	@Override
 	public List<BoardVO> list() throws Exception {
-		return sql.selectList(namespace + ".list");
+		return 
+				null;
+//				sql.selectList(namespace + ".list");
 	}
 
 	@Override
 	public void write(BoardVO vo) throws Exception {
-		sql.insert(namespace + ".write", vo);
+//		sql.insert(namespace + ".write", vo);
 	}
 
 	@Override
 	public BoardVO view(long tNo) throws Exception {
-		return sql.selectOne(namespace + ".view", tNo);
+		return 
+				null;
+//				sql.selectOne(namespace + ".view", tNo);
 	}
 	
 	@Override
 	public void modify(BoardVO vo) throws Exception {
-		sql.update(namespace + ".modify", vo);
+//		sql.update(namespace + ".modify", vo);
 	}
 
 	@Override
 	public void delete(long tNo) throws Exception {
-		sql.delete(namespace + ".delete", tNo);
+//		sql.delete(namespace + ".delete", tNo);
 	}
 }
